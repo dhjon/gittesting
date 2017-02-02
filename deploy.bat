5ff61f96d89919dee0fed895a9658f2ff8b94776
Jonathan <jonathanryan@gmail.com>
2017-02-02 08:52:06 -0500

    Add files via upload

---
+++ deploy.bat
@@ -0,0 +1,1 @@
+powershell .\deployDllToGac.ps1
---
+++ deployDllToGac.ps1
@@ -0,0 +1,27 @@
+﻿clear-host
+$execDir = Split-Path -parent $PSCommandPath
+
+$filesToDeploy = $(ls $(join-path $execDir "\dllsToDeploy")).FullName
+$nl = [Environment]::Newline
+
+$promptHelper = $filesToDeploy | %{
+    return $($_ + $nl)
+}
+
+$ans = read-host "Deploy these files to GAC? $nl $promptHelper (y/n)"
+
+if($ans.ToLower() -eq "y" -or $ans.ToLower() -eq "yes"){
+    $filesToDeploy | %{
+        $output = .\gacutil.exe -i $_
+        
+        if($output.Contains("Assembly successfully added to the cache")){
+            write-host -fore green -back black Successfully added $_
+        }
+        else{
+            write-host -fore red -back black "Error adding $_"
+        }
+    }
+}
+else{
+    write-host -fore red -back black "Aborting..."
+}
